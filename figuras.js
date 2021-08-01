79f813bf0efabca1524e631199cabd9dcd262656
j2pm <jpmjim@gmail.com>
2021-07-31 19:51:58 -0500

    Taller #1 Part 3 Interactuando JS con HTML, terminando el reto del triangulo y circulo

--- figuras.js
+++ figuras.js
@@ -25,6 +25,10 @@ function perimetroTriangulo(lado1, lado2, base){
 function areaTriangulo(base, altura){
   return (base * altura) / 2;
 }
+//Altura
+function alturaTriangulo (lado1, lado2, base){
+  return Math.sqrt((base/2) ** 2 + lado1 * lado2 );
+} 
 console.groupEnd();
 
 //Círculo
@@ -48,3 +52,66 @@ function areatroCirculo(radio){
   return radio ** 2 * PI;
 }
 console.groupEnd();
+
+//Creamos las funciones para los botones Calcular y Perímetro "Cuadrado"
+//=======================================================================
+function calcularPerimetroCuadrado(){
+  const input = document.getElementById("square__input");
+  const value = input.value;
+
+  const perimetro = perimetroCuadrado(value);
+  alert(perimetro);
+}
+function calcularAreaCuadrado(){
+  const input = document.getElementById("square__input");
+  const value = input.value;
+
+  const area = areaCuadrado(value);
+  alert(area);
+}
+
+//Creamos las funciones para los botones Calcular y Perímetro "Triángulo"
+//=======================================================================
+function calcularPerimetroTriangulo(){
+  const inputside1 = document.getElementById("triangle__input--side1");
+  const value1 = Number(inputside1.value);
+  const inputside2 = document.getElementById("triangle__input--side2");
+  const value2 = Number (inputside2.value);
+  const inputbase = document.getElementById("triangle__input--base");
+  const value3 = Number(inputbase.value);
+  const perimetro = perimetroTriangulo(value1,value2,value3);
+  alert("El perimetro del triangulo es: " + perimetro);
+}
+
+function calcularAreaTriangulo(){
+  const input1 = document.getElementById("triangle__input--side1");
+  const value1 = Number(input1.value);
+  const input2 = document.getElementById("triangle__input--side2");
+  const value2 = Number (input2.value);
+  const input3 = document.getElementById("triangle__input--base");
+  const value3 = Number(input3.value);
+  const altura = alturaTriangulo(value1, value2, value3);
+  const area = areaTriangulo(value3, altura);
+  alert("La altura del triangulo es: " + altura + " El area del triangulo es: " + area);
+}
+
+//Creamos las funciones para los botones Calcular y Perímetro "Triángulo"
+//=======================================================================
+function calcularDiametroCirculo(){
+  const input = document.getElementById("circle__radius");
+  const value = input.value;
+  const diametro = diametroCirculo(value);
+  alert ("El diámetro del circulo es: " + diametro);
+}
+function calcularPerimetroCirculo(){
+  const input = document.getElementById("circle__radius");
+  const value = input.value;
+  const perimetro = perimetroCirculo(value);
+  alert("El perimetro del circulo es: " + perimetro);
+}
+function calcularAreaCirculo(){
+  const input = document.getElementById("circle__radius");
+  const value = input.value;
+  const area = areatroCirculo(value);
+  alert("El área del circulo es: " + area);
+}
