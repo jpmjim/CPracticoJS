cbdf7d3265a2674ac68431868dbdf5b564ccba0b
j2pm <jpmjim@gmail.com>
2021-08-02 19:19:15 -0500

    Taller #2 Part 3, Escribiendo HTML desde JavaScript

--- descuentos.js
+++ descuentos.js
@@ -7,3 +7,16 @@ function calculaPrecioConDescuento(precio, descuento){
   const precioConDescuento =  (precio * porcentajePrecioConDescuento) / 100;
   return precioConDescuento;
 }
+
+//Funcion conectar con HTML
+function productPriceDiscount(){
+  const inputPrice = document.getElementById("product__price");
+  const priceValue = inputPrice.value;
+  const inputDiscount = document.getElementById("product__discount");
+  const discountValue = inputDiscount.value;
+  const priceWithDiscount = calculaPrecioConDescuento(priceValue, discountValue);
+
+  //utilizando innertext que es un atributo
+  const resultPrice = document.getElementById("ResultP");
+  resultPrice.innerText = "El precio con descuento son: s/" + priceWithDiscount;
+}
