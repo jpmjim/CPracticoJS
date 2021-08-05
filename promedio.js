24b1ac28a559c0a6a1a1b7c77f4b7fdcaaaa9405
j2pm <jpmjim@gmail.com>
2021-08-04 21:02:11 -0500

    Taller #3 Part2, creamos uan funcion que sacara el promedio de cualquier lista que se pueda ingresar

--- promedio.js
+++ promedio.js
@@ -1,13 +1,9 @@
-const lista1 = [
-  100,
-  200,
-  300,
-  500,
-];
+function calcularMediaAritmetica(lista){
+  let sumaLista = 0;
+  for (let i = 0; i < lista.length; i++) {
+    sumaLista = sumaLista + lista[i];
+  }
 
-let sumaLista1 = 0;
-for (let i = 0; i < lista1.length; i++) {
-  sumaLista1 = sumaLista1 + lista1[i];
+  const promedioLista = sumaLista / lista.length;
+  return promedioLista;
 }
-
-const promedioLista1 = sumaLista1 / lista1.length;
