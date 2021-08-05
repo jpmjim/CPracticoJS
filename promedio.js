d35c41071bf3b240fd07146aed2e77238b8307ba
j2pm <jpmjim@gmail.com>
2021-08-04 21:16:57 -0500

    Taller #3 Part3, Calculamos el promedio de una lista usando el metodo reduce

--- promedio.js
+++ promedio.js
@@ -1,8 +1,18 @@
 function calcularMediaAritmetica(lista){
-  let sumaLista = 0;
-  for (let i = 0; i < lista.length; i++) {
-    sumaLista = sumaLista + lista[i];
-  }
+  //Cuando trabajemos con array no solo podemos utilizar el ciclo for para iterar, tambien existen metodos.
+  // let sumaLista = 0;
+  // for (let i = 0; i < lista.length; i++) {
+  //   sumaLista = sumaLista + lista[i];
+  // }
+
+  //Metodos
+  //=========
+  //Reduce, va recibir una funcion que debemos enviar como argumento, el cula no permite sumar cadomo uno de los elemtos.
+  const sumaLista = lista.reduce(
+    function(valorAcumulado = 0, nuevoElemento){
+      return valorAcumulado + nuevoElemento;
+    }
+  );
 
   const promedioLista = sumaLista / lista.length;
   return promedioLista;
