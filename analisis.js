da5b40648a37244bb193d7c59acb000c874ae55a
j2pm <jpmjim@gmail.com>
2021-08-05 19:25:10 -0500

    Taller #4 Part2, hallando la mediana del top 10 de salarios en Col

--- analisis.js
+++ analisis.js
@@ -1,24 +1,11 @@
-const salarioCol = colombia.map(
-  function (personita){
-    return personita.salary;
-  }
-);
-
-//Ordenar el array
-const salarioColSorted = salarioCol.sort(
-  function(salaryA, salaryB){
-    return salaryA - salaryB;
-  }
-);
-
-//Mediana de salarios General
-function esPar(numerito){
+// Helpers
+function esPar(numerito) {
   return (numerito % 2 === 0);
-};
+}
 
-function calcularMediaAritmetica(lista){
+function calcularMediaAritmetica(lista) {
   const sumaLista = lista.reduce(
-    function(valorAcumulado = 0, nuevoElemento){
+    function (valorAcumulado = 0, nuevoElemento) {
       return valorAcumulado + nuevoElemento;
     }
   );
@@ -26,11 +13,14 @@ function calcularMediaAritmetica(lista){
   return promedioLista;
 }
 
-function medianaSalarios(lista){
+// Calculadora de mediana
+function medianaSalarios(lista) {
   const mitad = parseInt(lista.length / 2);
-  if (esPar(lista.length)){
+
+  if (esPar(lista.length)) {
     const personitaMitad1 = lista[mitad - 1];
     const personitaMitad2 = lista[mitad];
+
     const mediana = calcularMediaAritmetica([personitaMitad1, personitaMitad2]);
     return mediana;
   } else {
@@ -39,6 +29,36 @@ function medianaSalarios(lista){
   }
 }
 
-console.log(
-  medianaSalarios(salarioColSorted)
+// Mediana General
+const salariosCol = colombia.map(
+  function (personita) {
+    return personita.salary;
+  }
 );
+
+const salariosColSorted = salariosCol.sort(
+  function (salaryA, salaryB) {
+    return salaryA - salaryB;
+  }
+);
+
+const medianaGeneralCol = medianaSalarios(salariosColSorted);
+
+// Mediana del top 10%
+//metodo splice sirve para seleccionar elementos recibe dos parametros; 1 donde hacer el corte posicion, 2 cuantos elementos extraer para enviarlos a una array.
+const spliceStart = (salariosColSorted.length * 90) / 100;
+const spliceCount = salariosColSorted.length - spliceStart;
+
+const salariosColTop10 = salariosColSorted.splice(
+  spliceStart,
+  spliceCount,
+);
+
+const medianaTop10Col = medianaSalarios(salariosColTop10);
+
+console.log({
+  medianaGeneralCol,
+  medianaTop10Col,
+});
+
+
